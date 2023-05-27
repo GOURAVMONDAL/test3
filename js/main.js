46cfd434b4be4231a2d99589636140c12c338ac0
GOURAVMONDAL <gouravmondal805@gmail.com>
2023-05-27 17:46:04 +0530

    updt

--- js/main.js
+++ js/main.js
@@ -5,13 +5,13 @@ let development = document.querySelector('.dev');
 let heroImg = document.querySelector('.hero-img')
 
 design.addEventListener('mouseover', function() {
-    heroImg.src = '../images/hero-image.jpg'
+    heroImg.src = 'images/hero-image.jpg'
 })
 research.addEventListener('mouseover', function() {
-    heroImg.src = '../images/hero-image2.jpg'
+    heroImg.src = 'images/hero-image2.jpg'
 })
 development.addEventListener('mouseover', function() {
-    heroImg.src = '../images/hero-image3.jpg'
+    heroImg.src = 'images/hero-image3.jpg'
 })
 
 window.addEventListener('scroll', function() {
