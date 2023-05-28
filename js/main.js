757c8f7df658bf5501343de21ab1652f8225489a
GOURAVMONDAL <gouravmondal805@gmail.com>
2023-05-28 23:38:45 +0530

    update

--- js/main.js
+++ js/main.js
@@ -22,3 +22,65 @@ window.addEventListener('scroll', function() {
         this.document.querySelector('.navbar-content').classList.remove('background')
     }
 })
+
+
+let firstSolution = document.querySelector('.solution-block.one');
+let secondSolution = document.querySelector('.solution-block.two');
+let thirdSolution = document.querySelector('.solution-block.three');
+
+
+firstSolution.addEventListener('mouseover', function() {
+    if(!secondSolution.classList.contains('opacity')) {
+        secondSolution.classList.add('opacity')
+    } 
+    if(!thirdSolution.classList.contains('opacity')) {
+        thirdSolution.classList.add('opacity')
+    } 
+})
+firstSolution.addEventListener('mouseout', function() {
+    if(secondSolution.classList.contains('opacity')) {
+        secondSolution.classList.remove('opacity')
+    } 
+    if(thirdSolution.classList.contains('opacity')) {
+        thirdSolution.classList.remove('opacity')
+    } 
+})
+
+
+secondSolution.addEventListener('mouseover', function() {
+    if(!firstSolution.classList.contains('opacity')) {
+        firstSolution.classList.add('opacity')
+    } 
+    if(!thirdSolution.classList.contains('opacity')) {
+        thirdSolution.classList.add('opacity')
+    } 
+})
+secondSolution.addEventListener('mouseout', function() {
+    if(firstSolution.classList.contains('opacity')) {
+        firstSolution.classList.remove('opacity')
+    } 
+    if(thirdSolution.classList.contains('opacity')) {
+        thirdSolution.classList.remove('opacity')
+    } 
+})
+
+
+thirdSolution.addEventListener('mouseover', function() {
+    if(!firstSolution.classList.contains('opacity')) {
+        firstSolution.classList.add('opacity')
+    } 
+    if(!secondSolution.classList.contains('opacity')) {
+        secondSolution.classList.add('opacity')
+    } 
+})
+thirdSolution.addEventListener('mouseout', function() {
+    if(firstSolution.classList.contains('opacity')) {
+        firstSolution.classList.remove('opacity')
+    } 
+    if(secondSolution.classList.contains('opacity')) {
+        secondSolution.classList.remove('opacity')
+    } 
+})
+
+
+
